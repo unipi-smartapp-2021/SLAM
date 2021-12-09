1b4c9182b667d214183bd94e4a8c7bff390700e2
ste-pac <stefanopacchini@mail.com>
2021-12-09 19:17:26 +0100

    Minor update
    - Used `black` to format the `cone_mapping.py` script
    - Rewrote the `if` statement into the `cone_callback` in the previous file
    - Changed `cone_left` and `cone_right` into a simple list `cones` where 0 is left and 1 is right
    - Changed `current_pos` into `position`
    - Added the `y == 0` check for discarding cones

--- src/cone_mapping/scripts/cone_mapping.py
+++ src/cone_mapping/scripts/cone_mapping.py
@@ -1,65 +1,64 @@
 #!/usr/bin/env python3
+
 import rospy
 import math
 from std_msgs.msg import Float32MultiArray, String
 from geometry_msgs.msg import PoseStamped, Pose2D, PoseArray, Pose
 
 
-class ConeMapper():
+class ConeMapper:
     def __init__(self):
-        self.cone_left = PoseArray()
-        self.cone_right = PoseArray()
-        self.current_pos = PoseStamped()
+        #            [   Left    ,   Right    ]
+        self.cones = [PoseArray(), PoseArray()]
+        self.position = PoseStamped()
 
-        # subscribers
-        self.sub_pose = rospy.Subscriber("/pose_stamped", PoseStamped, callback=self.pose_callback)
-        self.sub_cone = rospy.Subscriber("/model/lidar/output", Float32MultiArray, callback=self.cone_callback)
+        # Subscribers
+        self.sub_pose = rospy.Subscriber(
+            "/pose_stamped", PoseStamped, callback=self.pose_callback
+        )
+        self.sub_cone = rospy.Subscriber(
+            "/model/lidar/output", Float32MultiArray, callback=self.cone_callback
+        )
 
-        # publishers
-        self.pub_right = rospy.Publisher("/cone_right", PoseArray, latch=True, queue_size=1)
-        self.pub_left = rospy.Publisher("/cone_left", PoseArray, latch=True, queue_size=1)
-
+        # Publishers
+        self.pub_right = rospy.Publisher(
+            "/cone_right", PoseArray, latch=True, queue_size=1
+        )
+        self.pub_left = rospy.Publisher(
+            "/cone_left", PoseArray, latch=True, queue_size=1
+        )
 
     def pose_callback(self, msg: PoseStamped):
-        self.current_pos = msg
-
+        self.position = msg
 
     def cone_callback(self, msg: Float32MultiArray):
-        cone2idx = {"x": 0, "y": 1, "z": 2}
-        thresh_noise = 3
+        axis2index = {"x": 0, "y": 1, "z": 2}
+        noise_threshold = 3
 
         # read each cone's position
         for i in range(0, len(msg.data), 3):
-            x = msg.data[i+cone2idx["x"]]
-            y = msg.data[i+cone2idx["y"]]
-
-            cone_pos = Pose()
-            cone_pos.position.x = self.current_pos.pose.position.x + x
-            cone_pos.position.y = self.current_pos.pose.position.y + y
+            x = msg.data[i + axis2index["x"]]
+            y = msg.data[i + axis2index["y"]]
 
             # filter the object if it's too distant
-            if cone_pos.position.x > self.current_pos.pose.position.x + thresh_noise or \
-               cone_pos.position.x < self.current_pos.pose.position.x - thresh_noise or \
-               cone_pos.position.y > self.current_pos.pose.position.y + thresh_noise or \
-               cone_pos.position.y < self.current_pos.pose.position.y - thresh_noise or \
-               x <= 0:
+            if x <= 0 or x > noise_threshold or y == 0 or abs(y) > noise_threshold:
                 continue
 
-            # distribute cone on left or right
-            if y > 0:
-                self.cone_right.poses.append(cone_pos)
-            elif y < 0:
-                self.cone_left.poses.append(cone_pos)
+            cone = Pose()
+            cone.position.x = self.position.pose.position.x + x
+            cone.position.y = self.position.pose.position.y + y
+            self.cones[y < 0].append(cone)
 
 
 def main():
-    cone_mapper = ConeMapper()
     rospy.init_node("cone_mapping")
+
     print("Start node cone_mapping")
+    cone_mapper = ConeMapper()
     rate = rospy.Rate(1)
     while not rospy.is_shutdown():
-        cone_mapper.pub_right.publish(cone_mapper.cone_right)
-        cone_mapper.pub_left.publish(cone_mapper.cone_left)
+        cone_mapper.pub_left.publish(cone_mapper.cones[0])
+        cone_mapper.pub_right.publish(cone_mapper.cones[1])
         rate.sleep()
 
 
